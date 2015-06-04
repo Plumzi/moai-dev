4c4147546220524c4dbef3238944a5f6945f7446
Patrick Meehan <patrick@ziplinegames.com>
2015-06-03 23:27:45 -0700

    added support for luminance alpha images and textures

--- regression-samples/png-sample/main.lua
+++ regression-samples/png-sample/main.lua
@@ -14,13 +14,20 @@ layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
 MOAISim.pushRenderPass ( layer )
 
+gfxQuad1 = MOAIGfxQuad2D.new ()
+gfxQuad1:setTexture ( "good.png" )
+gfxQuad1:setRect ( -64, -8, 64, 8 )
+
+prop1 = MOAIProp2D.new ()
+prop1:setDeck ( gfxQuad1 )
+prop1:setLoc ( 0, 32 )
+layer:insertProp ( prop1 )
+
 gfxQuad2 = MOAIGfxQuad2D.new ()
 gfxQuad2:setTexture ( "bad.png" )
--- gfxQuad2:setTexture ( "good.png" )
 gfxQuad2:setRect ( -64, -8, 64, 8 )
 
 prop2 = MOAIProp2D.new ()
 prop2:setDeck ( gfxQuad2 )
-prop2:setLoc(50,50)
+prop2:setLoc ( 0, -32 )
 layer:insertProp ( prop2 )
-
